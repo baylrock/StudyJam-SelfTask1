4624ddc1caaf1095405c373c9b913d10f89f30a7
Baylrock <skidi@i.ua>
2016-04-05 16:44:31 +0300

    Guesser game logick copleted

--- app/src/main/java/com/baylrock/trainingtasks/studyjamselftask/MainActivity.java
+++ app/src/main/java/com/baylrock/trainingtasks/studyjamselftask/MainActivity.java
@@ -2,9 +2,22 @@ package com.baylrock.trainingtasks.studyjamselftask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.baylrock.trainingtasks.studyjamselftask.games.Guesser;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+    private int guess_state = 1;
+    private Button[] guess_buts_array = new Button[12];
+    private boolean inGame = false;
+    private Toast main_toast;
+    Guesser guesser_game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,10 +25,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
-    public void startGueser (View view) {
+//
+    public void startGueser(View view) {
         setContentView(R.layout.guesser_layout);
+        guesser_game = new Guesser(this);
     }
 
+    public void guesser_click(View view) {
+        int id;
+        if ((id  = view.getId()) == getResources().getIdentifier("button14","id",this.getPackageName())) {
+            guesser_game.guesserInit();
+            return;
+        }
+        if (id == getResources().getIdentifier("button13","id",this.getPackageName())) {
+            //next
+            return;
+        }
+        guesser_game.guess(view);
+
+    }
 
 }
---
+++ app/src/main/java/com/baylrock/trainingtasks/studyjamselftask/games/Guesser.java
@@ -0,0 +1,134 @@
+package com.baylrock.trainingtasks.studyjamselftask.games;
+
+import android.app.Activity;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.baylrock.trainingtasks.studyjamselftask.R;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by baylrock on 05.04.2016.
+ */
+public class Guesser {
+
+    private Activity activity;
+    private int guess_state = 1;
+    private Button[] guess_buts_array = new Button[12];
+    private boolean inGame = false;
+    private Toast main_toast;
+    private int data[];
+
+    public Guesser(Activity activity) {
+        this.activity = activity;
+        startGueser();
+    }
+
+
+
+
+    public void startGueser() {
+
+        for (int i = 1; i < 13; i++) {
+            if ((guess_buts_array[i - 1] = (Button) activity.findViewById(activity.getResources().getIdentifier("button" + i, "id", activity.getPackageName()))) == null) {
+                Log.e("Cant find butt with id", "button" + i);
+                return;
+            }
+        }
+    }
+
+    public void guesserInit() {
+        Button guess_bt;
+        inGame = false;
+        data  = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+
+        guess_state = 1;
+        gameSort(data);
+        for (int i = 0; i < 12; i++) {
+            guess_bt = guess_buts_array[i];
+            Log.d("Check butt", (String) guess_bt.getText());
+            guess_bt.setText("" + data[i]);
+            guess_bt.setEnabled(true);
+        }
+        new Timer().schedule(new GamePause(), 5000);
+    }
+
+    public void guess(View view) {
+        if (!inGame) return;
+        Button guess_but = (Button) view;
+        if (guess_but.equals(guess_buts_array[guess_state - 1])) {
+            if (guess_state == 12) {
+                if (main_toast != null) main_toast.cancel();
+                main_toast = Toast.makeText(activity.getApplicationContext(), "YOU WIN!", Toast.LENGTH_SHORT);
+                main_toast.show();
+                setGuesButtsEnabled(false);
+
+            } else {
+                if (main_toast != null) main_toast.cancel();
+                main_toast = Toast.makeText(activity.getApplicationContext(), "Ready " + (guess_state) + "/12", Toast.LENGTH_SHORT);
+                main_toast.show();
+
+            }
+
+        } else {
+            setGuesButtsEnabled(false);
+            Toast.makeText(activity.getApplicationContext(), "You loose!", Toast.LENGTH_LONG).show();
+            for (int i = guess_state-1; i < 12; i++) {
+                guess_buts_array[i].setText("" + data[i]);
+                guess_buts_array[i].setTextColor(Color.RED);
+            }
+            return;
+        }
+
+        guess_but.setText("" + guess_state++);
+    }
+
+    private void setGuesButtsEnabled(boolean state) {
+        for (int i = 0; i < 12; i++) {
+            guess_buts_array[i].setEnabled(state);
+        }
+    }
+
+    private void setGuesButtsText(String text) {
+        for (int i = 0; i < 12; i++) {
+            guess_buts_array[i].setText(text);
+        }
+    }
+
+
+    class GamePause extends TimerTask {
+        @Override
+        public void run() {
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    setGuesButtsText("");
+                    inGame = true;
+                }
+            });
+
+        }
+    }
+
+    private void gameSort(int[] data) {
+        Random rnd = new Random();
+        for (int i = 0, buff, randIndex; i < data.length; i++) {
+            buff = data[i];
+            data[i] = data[randIndex = rnd.nextInt(data.length)];
+            data[randIndex] = buff;
+        }
+        this.data = data;
+    }
+
+
+
+
+}
